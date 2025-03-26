b8bc71af4d2e516294146eea6b41203527dde05f
AlizaFatima5 <alizafatimaa34@gmail.com>
2025-03-26 00:27:16 -0700

    Update server.py

--- server.py
+++ server.py
@@ -7,4 +7,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "BookYourBarber.settings")
 application = get_asgi_application()
 
 if __name__ == "__main__":
-    server.run("BookYourBarber.asgi:application", port=8000, host="0.0.0.0")
+    server.run("BookYourBarber.asgi:application", port=8000, host="127.0.0.1")
